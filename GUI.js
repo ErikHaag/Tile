3608effba86f72c908d19bffc49d9a49601abdb5
ErikHaag <60990391+ErikHaag@users.noreply.github.com>
2020-03-11 19:16:52 -0600

    Update GUI.js

--- GUI.js
+++ GUI.js
@@ -2,6 +2,6 @@ let rowS
 let colS;
 
 function setupGUI() {
-  rowS = createSlider(1,10,4,1);
-  colS = createSlider(1,10,4,1);
+  rowS = createSlider(2,10,4,1);
+  colS = createSlider(2,10,4,1);
 }
